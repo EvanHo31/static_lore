b70a879ef8ce43bfa800ca074f529bb321972c78
EvanHo31 <leorix31@gmail.com>
2026-05-09 16:27:32 +0800

    leafnode

---
+++ src/test_leafnode.py
@@ -0,0 +1,19 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_url(self):
+        node = LeafNode("a", "click me", props={"href":"https://google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://google.com">click me</a>')
+    
+    def test_h1(self):
+        node = LeafNode("h1", "Header Title")
+        self.assertEqual(node.to_html(), '<h1>Header Title</h1>')
+    
+    def test_repr(self):
+        node = LeafNode("a", "click me", props={"href":"https://google.com"})
+        self.assertEqual(str(node), "LeafNode(a, click me, {'href': 'https://google.com'})")
