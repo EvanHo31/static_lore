e2b0b85317ccb65f9f55222a36356ae814eba2fe
EvanHo31 <leorix31@gmail.com>
2026-05-12 21:27:05 +0800

    add more pages

--- src/main.py
+++ src/main.py
@@ -5,5 +5,9 @@ from generate import generate_page
 def main():
     copy_static()
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+    generate_page("content/blog/glorfindel/index.md", "template.html", "public/blog/glorfindel/index.html")
+    generate_page("content/blog/tom/index.md", "template.html", "public/blog/tom/index.html")
+    generate_page("content/blog/majesty/index.md", "template.html", "public/blog/majesty/index.html")
 
 main()
