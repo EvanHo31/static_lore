bf08ffc260a6164b513b68d8a09d51d6ce5b5932
EvanHo31 <leorix31@gmail.com>
2026-05-12 17:32:19 +0800

    removed white space clean up

--- src/leafnode.py
+++ src/leafnode.py
@@ -8,12 +8,9 @@ class LeafNode(HTMLNode):
     def to_html(self):
         if self.value is None:
             raise ValueError("A leafnode must have a value")
-        
-        text = str(self.value)
-        text = re.sub(r"\s+", " ", text)
         if not self.tag:
-            return text
-        return f"<{self.tag}{self.props_to_html()}>{text}</{self.tag}>"
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
