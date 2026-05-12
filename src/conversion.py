169f2ab2f3813b69589cacab14c10e6ce335ca9b
EvanHo31 <leorix31@gmail.com>
2026-05-12 17:55:46 +0800

    markdown to html node

--- src/conversion.py
+++ src/conversion.py
@@ -1,6 +1,9 @@
 from textnode import TextNode, TextType
 from leafnode import LeafNode
+from parentnode import ParentNode
 from extraction import split_nodes_image, split_nodes_link, split_nodes_delimiter
+from blocks import block_to_block_type, BlockType
+import re
 
 def text_node_to_html_node(text_node:TextNode):
     tag = ""
@@ -53,3 +56,99 @@ def markdown_to_blocks(text:str):
         else:
             block += line + "\n"
     return blocks
+
+def clean_white_spaces(text:str):
+    # text = text.strip()
+    text = re.sub(r"\s+", " ", text)
+    return text
+
+def paragrpah_to_html_node(block:str):
+    textnodes = text_to_text_nodes(block)
+    html_nodes = []
+    for node in textnodes:
+        node.text = clean_white_spaces(node.text)
+        html_nodes.append(text_node_to_html_node(node))
+    paragraph = ParentNode("p", html_nodes)
+    return paragraph
+
+def code_to_html_node(block:str):
+    textnode = TextNode(block[4:-3], TextType.CODE)
+    return ParentNode("pre", [text_node_to_html_node(textnode)])
+
+def header_to_html_node(block:str):
+    size = 1
+    while block[size] != " ": size+=1
+    textnodes = text_to_text_nodes(block[size+1:])
+    html_nodes = []
+    for node in textnodes:
+        # node.text = clean_white_spaces(node.text)
+        html_nodes.append(text_node_to_html_node(node))
+    header = ParentNode(f"h{size}", html_nodes)
+    return header
+
+def quote_to_html_node(block:str):
+    html_nodes = []
+    lines = block.split("\n")
+    for line in lines:
+        line = line[1:]
+        text_nodes = text_to_text_nodes(line)
+        nodes = []
+        for node in text_nodes:
+            nodes.append(text_node_to_html_node(node))
+        html_nodes.append(ParentNode("p", nodes))
+    quote = ParentNode("blockquote", html_nodes)
+    return  quote
+
+def ulist_to_html_node(block:str):
+    list_item = []
+    lines = block.split("\n")
+    for line in lines:
+        line = line[2:]
+        text_nodes = text_to_text_nodes(line)
+        nodes = []
+        for node in text_nodes:
+            nodes.append(text_node_to_html_node(node))
+        list_item.append(ParentNode("li", nodes))
+    return ParentNode("ul", list_item)
+
+def olist_to_html_node(block:str):
+    list_item = []
+    lines = block.split("\n")
+    for line in lines:
+        line = line[3:]
+        text_nodes = text_to_text_nodes(line)
+        nodes = []
+        for node in text_nodes:
+            nodes.append(text_node_to_html_node(node))
+        list_item.append(ParentNode("li", nodes))
+    return ParentNode("ol", list_item)
+
+def markdown_to_html_node(markdown:str):
+    blocks = markdown_to_blocks(markdown)
+    html_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        match block_type:
+            case BlockType.PARAGRAPH:
+                html_nodes.append(paragrpah_to_html_node(block))
+
+            case BlockType.CODE:
+                html_nodes.append(code_to_html_node(block))
+            
+            case BlockType.HEADING:
+                html_nodes.append(header_to_html_node(block))
+            
+            case BlockType.QUOTE:
+                html_nodes.append(quote_to_html_node(block))
+
+            case BlockType.UNORDERED_LIST:
+                html_nodes.append(ulist_to_html_node(block))
+
+            case BlockType.ORDERED_LIST:
+                html_nodes.append(olist_to_html_node(block))
+
+            case _:
+                raise ValueError("invalid block type")
+            
+    div = ParentNode("div", html_nodes)
+    return div
--- src/test_conversion.py
+++ src/test_conversion.py
@@ -1,5 +1,5 @@
 import unittest
-from conversion import text_node_to_html_node, text_to_text_nodes, markdown_to_blocks
+from conversion import text_node_to_html_node, text_to_text_nodes, markdown_to_blocks, markdown_to_html_node
 from textnode import TextNode, TextType
 
 class Test_test_node_to_html_node(unittest.TestCase):
@@ -129,4 +129,112 @@ class Test_Markdown_Converstion(unittest.TestCase):
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+class Test_Markdown_To_HTML_Node(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_headerblock(self):
+        md = """
+# This is text that _should_ remain
+the **same** even with inline stuff
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>This is text that <i>should</i> remain\nthe <b>same</b> even with inline stuff</h1></div>",
+        )
+    
+    def test_headerblock_h4(self):
+        md = """
+#### This is text that _should_ remain
+the **same** even with inline stuff
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h4>This is text that <i>should</i> remain\nthe <b>same</b> even with inline stuff</h4></div>",
+        )
+
+    def test_headerblock_fali(self):
+        md = """
+####### This is text that _should_ remain
+the **same** even with inline stuff
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>####### This is text that <i>should</i> remain the <b>same</b> even with inline stuff</p></div>",
+        )
+    
+    def test_quoteblock(self):
+        md = """
+>This is text that _should_ remain
+> the **same** even with inline stuff
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote><p>This is text that <i>should</i> remain</p><p> the <b>same</b> even with inline stuff</p></blockquote></div>",
+        )
+
+    def test_ulist_block(self):
+        md = """
+- This is text that _should_ remain
+-  the **same** even with inline stuff
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is text that <i>should</i> remain</li><li> the <b>same</b> even with inline stuff</li></ul></div>",
+        )
+    
+    def test_olist_block(self):
+        md = """
+1. This is text that _should_ remain
+2.  the **same** even with inline stuff
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>This is text that <i>should</i> remain</li><li> the <b>same</b> even with inline stuff</li></ol></div>",
         )
